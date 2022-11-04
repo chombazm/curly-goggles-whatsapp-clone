a0929d7f32fa5da44abebc21afc2bce4f34d618c
chomba <chomba@bongohive.co.zm>
2022-11-04 23:35:43 +0200

    Add camera

--- screens/CameraScreen.tsx
+++ screens/CameraScreen.tsx
@@ -1,19 +1,32 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { Camera, CameraType } from 'expo-camera';
 
 export const CameraScreen = () => {
+  const [type, setType] = React.useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  // request permission
+  React.useEffect(() => {
+    requestPermission();
+  }, []);
+
+  console.log({ permission });
   return (
     <View style={styles.container}>
-      <Text>CameraScreen</Text>
+      <Camera style={styles.camera} type={type} />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    
-  }
-})
+  },
+  camera: {
+    width: '100%',
+    height: '100%',
+  },
+});
